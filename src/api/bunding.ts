32f9a43b230d969ac7220846f659f4dd88cafe21
hzj <hezhijian249@qq.com>
2024-11-10 22:19:01 +0800

    feat: 项目的base设置后请求地址也同步更改

--- src/api/bunding.ts
+++ src/api/bunding.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export function getPizzaCollaborationBpmn() {
-  return axios.get('/bpmn/pizza-collaboration.bpmn')
+  return axios.get('/bpmn-designer/bpmn/pizza-collaboration.bpmn')
 }
